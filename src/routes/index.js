6e4718778912acf20a97f6f189a7c1cba9bd9940
Vínicius Santos <viniciuszero@Live.com>
2023-04-25 17:36:02 -0300

    :art: (routes) Gathering all routes exports

---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+const authorsRoutes = require('./authors.routes')
+const authenticateRoutes = require('./authenticate.routes')
+const newsRoutes = require('./news.routes')
+
+
+module.exports = {
+  authorsRoutes,
+  authenticateRoutes,
+  newsRoutes
+}
